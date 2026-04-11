f100340f4e5bc2dca41fd2bd78bd5e4beea8e403
alskdj1234 <qpalzm7714@gmail.com>
2026-04-11 18:32:46 +0900

    dd

--- spring05quiz/src/main/java/com/kh/spring05quiz/quizcontroller/QuizController.java
+++ spring05quiz/src/main/java/com/kh/spring05quiz/quizcontroller/QuizController.java
@@ -1,5 +1,34 @@
 package com.kh.spring05quiz.quizcontroller;
 
+import java.time.LocalDate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class QuizController {
 
+    @RequestMapping("/quiz05")
+    public String quiz05(
+            
+            @RequestParam(required = false, defaultValue = "2007") int year) {
+        
+        int currentYear = LocalDate.now().getYear();
+        int age = currentYear - year + 1; 
+        
+        int price;
+        if (age >= 65 || age < 8) {
+            price = 0; 
+        } else if (age >= 20) {
+            price = 1400; 
+        } else if (age >= 14) {
+            price = 800; 
+        } else if (age >= 8) {
+            price = 500; 
+        } else {
+            price = 0;
+        }
+
+        return " 나이: " + age + "세, 요금: " + price + "원";
+    }
 }
